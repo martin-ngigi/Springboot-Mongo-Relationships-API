dc06185aa75fbb90e9205924d1e526822a3180c9
martin-ngigi <martinwainaina001@gmail.com>
2022-12-29 12:27:59 +0300

    Initial commit

---
+++ src/main/java/com/martin/mongorelationships/MongoRelationshipsApplication.java
@@ -0,0 +1,13 @@
+package com.martin.mongorelationships;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MongoRelationshipsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MongoRelationshipsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/martin/mongorelationships/MongoRelationshipsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.martin.mongorelationships;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MongoRelationshipsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
